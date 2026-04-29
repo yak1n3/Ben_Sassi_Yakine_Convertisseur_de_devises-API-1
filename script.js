ba478d6b72b4c6f966a08e8a3dc50195e88badd4
yak1n3 <yakinbensassi7@gmail.com>
2026-04-29 19:06:36 +0000

    Add files via upload
    Ajout conversion avec API

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const amount = document.getElementById("amount");
+const from = document.getElementById("from");
+const to = document.getElementById("to");
 const result = document.getElementById("result");
 
-function convert() {
-  result.innerText = amount.value;
+async function convert() {
+  const montant = parseFloat(amount.value);
+  if (isNaN(montant) ||montant <= 0) return;
+
+  const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${from.value}`);
+  const data = await response.json();
+
+  const rate = data.rates[to.value];
+  const total = montant * rate;
+
+  result.innerText = `${montant.toFixed(2)} ${from.value} = ${total.toFixed(2)} ${to.value}`;
 }
